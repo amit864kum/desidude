f6e6b8d5cc8079955660146c220f1128b86caaa8
AMIT KUMAR <111384406+amit864kum@users.noreply.github.com>
2023-01-20 17:27:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+const btnHam = document.querySelector('.ham-btn');
+const btnTimes = document.querySelector('.times-btn');
+const navBar = document.getElementById('nav-bar');
+
+btnHam.addEventListener('click', function(){
+    if(btnHam.className !== ""){
+        btnHam.style.display = "none";
+        btnTimes.style.display = "block";
+        navBar.classList.add("show-nav");
+    }
+})
+
+btnTimes.addEventListener('click', function(){
+    if(btnHam.className !== ""){
+        this.style.display = "none";
+        btnHam.style.display = "block";
+        navBar.classList.remove("show-nav");
+    }
+})
+
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true
+});
+
+const options = {
+    bottom: '32px', // default: '32px'
+    left: 'unset', // default: '32px'
+    right: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    // autoMatchOsTheme: true // default: true
+}
+
+
+const darkmode = new Darkmode(options);
+
+darkmode.showWidget();
